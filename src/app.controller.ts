c22cd84c23f691ffa3cf3d36f51ce9f11a1da3c5
Vanessa Isidro <vnisidro@traxiontech.net>
2023-08-15 16:17:28 +0800

    Transferred business logic to service.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,43 +1,37 @@
-import { Controller, Get, Post, Put, Delete, Param, Body, NotFoundException } from "@nestjs/common"
-import { v4 as uuid } from "uuid";
+import { Controller, Get, Post, Put, Delete, Param, Body, HttpCode } from "@nestjs/common"
 import { ReportType, data } from 'src/data'
+import { AppService } from "./app.service";
 
 // Class that allows us to create different endpoints
 @Controller('report/:type')
 export class AppController {
 
+  constructor(private appService: AppService){}
+
   @Get()
   getAllReports(@Param('type') type: string) {
     // console.log(type)
     const reportType = (type === "income") ? ReportType.INCOME : ReportType.EXPENSE;
-    return data.report.filter(report => report.type === reportType)
+    return this.appService.getAllReports(reportType);
   }
 
   @Get(':id')
-  getOneReport(@Param('type') type: string, @Param('id') id: string) {
+  getOneReport(
+    @Param('type') type: string, 
+    @Param('id') id: string
+  ) {
     const reportType = (type === "income") ? ReportType.INCOME : ReportType.EXPENSE;
-    return data.report
-      .filter((report) => report.type === reportType)
-      .find((report) => report.id === id)
+    return this.appService.getOneReport(reportType, id);
   }
 
   @Post()
   //createReport(@Body() body: {amount:number, source: string}) {
-    createReport(
-      @Body() { amount, source }: { amount:number, source: string }, 
-      @Param('type') type: string
-    ) {
-    // console.log({ amount, source })
-    const newReport = {
-      id: uuid(),
-      source, // source: body.source,
-      amount, // amount: body.amount,
-      created_at: new Date(),
-      updated_at: new Date(),
-      type: (type === "income") ? ReportType.INCOME : ReportType.EXPENSE
-    }
-    data.report.push(newReport);
-    return newReport;
+  createReport(
+    @Body() { amount, source }: { amount:number, source: string }, 
+    @Param('type') type: string
+  ) {
+    const reportType = (type === "income") ? ReportType.INCOME : ReportType.EXPENSE;
+    return this.appService.createReport(reportType, { amount, source })
   }
 
   @Put(':id')
@@ -45,29 +39,16 @@ export class AppController {
     @Param('id') id: string, 
     @Param('type') type: string,
     @Body() body: {amount:number, source: string}
-    ) {
-    const reportType = type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
-    const reportToUpdate = data.report
-      .filter((report) => report.type === reportType)
-      .find((report) => report.id === id)
-    
-      if(!reportToUpdate) throw new NotFoundException('Report not found.')
-
-      const reportIndex = data.report.findIndex((report) => report.id === reportToUpdate.id);
-
-      data.report[reportIndex] = {
-        // ellipses grabs all values of that array/object
-        ...data.report[reportIndex],
-        ...body
-      }
-
-      console.log(data.report[reportIndex])
-      return data.report[reportIndex]
+  ) {
+      const reportType = type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+      return this.appService.updateReport(reportType, id, body)
   }
 
+  @HttpCode(204)
   @Delete(':id')
-  deleteReport(@Param('id') id: string, @Param('type') type: string) {
-    const reportIndex = data.report.findIndex((report) => report.id === id);
-    return data.report.splice(reportIndex, 1);
+  deleteReport(
+    @Param('id') id: string, 
+  ) {
+    return this.appService.deleteReport(id);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,66 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common/decorators";
+import { NotFoundException } from "@nestjs/common/exceptions";
+import { ReportType, data } from "./data";
+import { v4 as uuid } from "uuid";
+
+interface Report {
+  source: string;
+  amount: number;
+}
 
 @Injectable()
+// Class that stores all business logic
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getAllReports(type: ReportType) {
+    return data.report.filter(report => report.type === type);
+  }
+
+  getOneReport(type: ReportType, id: string) {
+    return data.report
+      .filter((report) => report.type === type)
+      .find((report) => report.id === id)
+  }
+
+  createReport(type: ReportType, {amount, source}: Report) {
+    // console.log({ amount, source })
+    const newReport = {
+      id: uuid(),
+      source, // source: body.source,
+      amount, // amount: body.amount,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type,
+    }
+    data.report.push(newReport);
+    return newReport;
+  }
+
+  updateReport(type: ReportType, id: string, body: Report
+  ) {
+    const reportToUpdate = data.report
+      .filter((report) => report.type === type)
+      .find((report) => report.id === id)
+    
+      if(!reportToUpdate) throw new NotFoundException('Report not found.')
+   // const reportIndex = data.report.findIndex((report) => {report.id === reportToUpdate.id}); <= ERROR
+      const reportIndex = data.report.findIndex((report) => report.id === reportToUpdate.id);
+
+      data.report[reportIndex] = {
+        // ellipses grabs all values of that array/object
+        ...data.report[reportIndex],
+        ...body,
+        updated_at: new Date()
+      }
+
+      console.log(data.report[reportIndex])
+      return data.report[reportIndex]
+  }
+
+  deleteReport(id: string) {
+    const reportIndex = data.report.findIndex((report) => report.id === id);
+
+    if(reportIndex === -1) throw new NotFoundException('report not found')
+
+    data.report.splice(reportIndex, 1);
   }
 }
