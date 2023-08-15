6e853b860025fe3017534d2932e1148bf9352d4b
Vanessa Isidro <vnisidro@traxiontech.net>
2023-08-15 14:17:33 +0800

    Created endpoint for creating a report

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get, Post, Put, Delete, Param } from "@nestjs/common"
-import { report } from "process"
+import { Controller, Get, Post, Put, Delete, Param, Body } from "@nestjs/common"
+import { v4 as uuid } from "uuid";
 import { ReportType, data } from 'src/data'
 
 // Class that allows us to create different endpoints
@@ -8,6 +8,7 @@ export class AppController {
 
   @Get()
   getAllReports(@Param('type') type: string) {
+    // console.log(type)
     const reportType = (type === "income") ? ReportType.INCOME : ReportType.EXPENSE;
     return data.report.filter(report => report.type === reportType)
   }
@@ -21,8 +22,19 @@ export class AppController {
   }
 
   @Post()
-  createReport() {
-    return 'created'
+  //createReport(@Body() body: {amount:number, source: string}) {
+    createReport(@Body() {amount, source}: {amount:number, source: string}, @Param('type') type: string) {
+    // console.log({ body })
+    const newReport = {
+      id: uuid(),
+      source, // source: body.source,
+      amount, // amount: body.amount,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: (type === "income") ? ReportType.INCOME : ReportType.EXPENSE
+    }
+    data.report.push(newReport);
+    return 'created' 
   }
 
   @Put(':id')
